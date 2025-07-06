7806d8d327edcd7f94f46c217a215e9b3a9ac35c
olababiuch <ola.babiuch7@gmail.com>
2025-07-06 13:32:48 +0200

    part A task 2 completed

--- src/PartA1.java
+++ src/PartA1.java
@@ -1,8 +1,21 @@
 public class PartA1 {
     public static void main(String[] args) {
+
+        //Task 1
         for(int r=0;r<=30;r++)
         {
             System.out.print(r + " ");
         }
+        System.out.println("\n");
+
+        //Task 2
+        for(int r=30;r>=0;r--)
+            {
+            System.out.print(r + " ");
+            }
+        System.out.println("/n");
+
+        //Task 3
+
     }
 }
